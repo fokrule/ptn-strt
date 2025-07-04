61bd463cf9c73d700fe8ecd8f56e64db20eeea20
fokrule <fokrule.hasan@gmail.com>
2025-07-04 12:06:55 -0400

    added env

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql@localhost/py_starter'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Encapsulation
+class AgeAdvisor:
+    def __init__(self, age):
+        self._age = age #protected variable
+
+    def get_message(self):
+        if self._age < 19:
+            return "Young Kid!"
+        elif self._age <=25:
+            return "Hardworking phase"
+        else:
+            return "Result phase"
+        
+# Inheritance
+class TeenAdvisor(AgeAdvisor):
+    def get_message(self):
+        return "You are a tennager."
+    
+# Polumorphism
+def get_advice(age):
+    if 13 <= age <= 17:
+        advisor = TeenAdvisor(age)
+    else:
+        advisor = AgeAdvisor(age)
+    return advisor.get_message()
+
 @app.route('/')
 def Index() :
     age = 19
-    if age < 19:
-        message = "young kid!"
-    elif age >= 19 and age <= 25:
-        message = "Hardworking phase of your life!"
-    else :
-        message = "Well try hard and start getting result"
+    message = get_advice(age)
 
 
     # Match section
