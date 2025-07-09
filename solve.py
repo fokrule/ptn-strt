36f6f7938dbd48aef3ef2daa96f97aa7f1528a7f
fokrule <fokrule.hasan@gmail.com>
2025-07-09 12:32:04 -0400

    working on letcode 2

--- solve.py
+++ solve.py
@@ -1,1 +1,9 @@
-print("--------")
+l1 = [2,4,3]
+l2 = [5,6,4]
+output = []
+reminder = 0
+for index in range(0, len(l1)):
+    if ((l1[index] + l2[index]) // 10 != 0):
+        reminder = (l1[index] + l2[index]) // 10
+    output.append((reminder + l1[index] + l2[index]) %10)
+print(output[::-1])
